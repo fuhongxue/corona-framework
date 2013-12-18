4250f1c8d5e1ae69e398081fd349fdd5ec2f4c48
fakestarbaby <fakestarbaby@gmail.com>
2013-12-18 14:55:36 +0900

    Update ultimate config.lua

--- config.lua
+++ config.lua
@@ -1,29 +1,110 @@
-application = {
-	content = {
-		width = 320,
-		height = 480, 
-		scale = "letterBox",
-		fps = 30,
-		
-		--[[
-        imageSuffix = {
-		    ["@2x"] = 2,
-		}
-		--]]
-	},
+-- iPad
+if string.sub(system.getInfo("model"), 1, 4) == "iPad" then
+  application = {
+    content = {
+      width  = 360,
+      height = 480,
+      fps    = 60,
+      scale  = "letterBox",
+      xAlign = "center",
+      yAlign = "center",
+      imageSuffix = {
+        ["@2x"] = 1.5,
+        ["@4x"] = 3.0,
+      },
+    },
+    notification = {
+      iphone = {
+        types = { "badge", "sound", "alert", }
+      },
+    },
+  }
 
-    --[[
-    -- Push notifications
+-- iPhone5
+elseif string.sub(system.getInfo("model"), 1, 2) == "iP" and 960 < display.pixelHeight then
+  application = {
+    content = {
+      width  = 320,
+      height = 568,
+      fps    = 60,
+      scale  = "letterBox",
+      xAlign = "center",
+      yAlign = "center",
+      imageSuffix = {
+        ["@2x"] = 1.5,
+        ["@4x"] = 3.0,
+      },
+    },
+    notification = {
+      iphone = {
+        types = { "badge", "sound", "alert", }
+      },
+    },
+  }
 
-    notification =
-    {
-        iphone =
-        {
-            types =
-            {
-                "badge", "sound", "alert", "newsstand"
-            }
-        }
-    }
-    --]]    
-}
+-- iPhone 3,4 and Older iPod Touch
+elseif string.sub(system.getInfo("model"), 1, 2) == "iP" then
+  application = {
+    content = {
+      width  = 320,
+      height = 480,
+      fps    = 60,
+      scale  = "letterBox",
+      xAlign = "center",
+      yAlign = "center",
+      imageSuffix = {
+          ["@2x"] = 1.5,
+          ["@4x"] = 3.0,
+      },
+    },
+    notification = {
+      iphone = {
+        types = { "badge", "sound", "alert", }
+      },
+    },
+  }
+
+-- Android and Kindle Fire and Nook
+elseif display.pixelHeight / display.pixelWidth > 1.72 then
+  application = {
+    content = {
+      width  = 320,
+      height = 570,
+      fps    = 60,
+      scale  = "letterBox",
+      xAlign = "center",
+      yAlign = "center",
+      imageSuffix = {
+        ["@2x"] = 1.5,
+        ["@4x"] = 3.0,
+      },
+    },
+    notification = {
+      google = {
+        projectNumber = "xxxxxxxxxxxx",
+      },
+    },
+  }
+
+-- Other
+else
+  application = {
+    content = {
+      width  = 320,
+      height = 512,
+      fps    = 60,
+      scale  = "letterBox",
+      xAlign = "center",
+      yAlign = "center",
+      imageSuffix = {
+        ["@2x"] = 1.5,
+        ["@4x"] = 3.0,
+      },
+    },
+    notification = {
+      google = {
+        projectNumber = "xxxxxxxxxxxx",
+      },
+    },
+  }
+end
