560d3570bfcb03e54b8a5da24beb989cae659c46
fakestarbaby <fakestarbaby@gmail.com>
2013-12-18 15:44:17 +0900

    Add system event listener

--- main.lua
+++ main.lua
@@ -1,6 +1,16 @@
 -- Hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
+-- System event listener
+local function applicationSystemEventListener(event)
+  if event.type == "applicationStart" then
+    print(event.type)
+  elseif event.type == "applicationExit" then
+    print(event.type)
+  end
+end
+Runtime:addEventListener("system", applicationSystemEventListener)
+
 -- Styoryboard
 local storyboard = require "storyboard"
 storyboard.purgeOnSceneChange = true
